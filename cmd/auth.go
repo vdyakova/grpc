e18532cf03051aa9418b95fd886e61cec54c3aeb
vdyakova <dyackova.valerya@yandex.ru>
2024-09-11 17:09:39 +0300

    update

--- cmd/auth.go
+++ cmd/auth.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	address = "localhost:50051"
+	address = "host.docker.internal:50051"
 	noteID  = 10
 )
 
@@ -21,16 +21,29 @@ func main() {
 		log.Fatalf("failed to connect to server: %v", err)
 	}
 	defer conn.Close()
-
 	c := desc.NewNoteV1Client(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	createResp, err := c.Create(ctx, &desc.CreateRequest{
+		Name:            "Lera",
+		Email:           "email",
+		Password:        "password",
+		PasswordConfirm: "passwordconfirm",
+		Role:            desc.Role_USER, // 0 соответствует USER
+	})
+	if err != nil {
+		log.Fatalf("failed to create note: %v", err)
+	}
+
+	// Вывод ID созданной заметки
+	log.Printf("note created with ID: %v", createResp.Id)
+
+	getResp, err := c.Get(ctx, &desc.GetRequest{Id: createResp.Id})
 	if err != nil {
 		log.Fatalf("failed to get note by id: %v", err)
 	}
 
-	log.Printf(("Note info:\n"), ("%+v"), r)
+	log.Printf("Note info: %+v", getResp)
 }
